fbc7d087e73af3b4b297541a2e737f609c8ae72d
Zachary Lentz <zlentz@slac.stanford.edu>
2017-12-06 13:40:02 -0800

    ENH: Revise to reflect the granularity of which the modules can operate

--- test_step.py
+++ test_step.py
@@ -2,20 +2,20 @@
 import sys
 import importlib
 
+
 def import_shim(name):
     """
-    Returns an array of the four functions we're expecting from the module
+    Returns an array of the functions we're expecting from the module
     we're currently testing.
-    1. get_pv(pvname): returns (pv object, conn Event) where the pv
-                       object is set up to mark the Event flag at the
-                       appropriate time.
-    2. connect(pvobj): starts connecting the pv object
-    3. get(pvobj): gets and returns the value
-    4. monitor_test(pvname): sets up the object to monitor pvname and sum the
+    1. get_pv(pvname): returns pv object,  where the pv
+                       object is fully connected
+    2. get(pvobj): gets and returns the value
+    3. monitor_test(pvname): sets up the object to monitor pvname and sum the
                              elements
     """
     module = importlib.import_module(name + "_benchmark")
-    return [module.get_pv, module.connect, module.get, module.monitor_test]
+    return [module.get_pv, module.get, module.monitor_test]
+
 
 if __name__ == "__main__":
     module_name = sys.argv[1]
@@ -24,15 +24,10 @@ if __name__ == "__main__":
     if test_level > 0:
         funcs = import_shim(module_name)
         get_pv = funcs[0]
-        connect = funcs[1]
-        get = funcs[2]
+        get = funcs[1]
     if test_level > 1:
-        pvobj, conn_ev = get_pv(pvname)
+        pvobj = get_pv(pvname)
     if test_level > 2:
-        connect(pvobj)
-    if test_level > 3:
-        conn_ev.wait()
-    if test_level > 4:
         value = None
         while value is None:
             value = get(pvobj)
